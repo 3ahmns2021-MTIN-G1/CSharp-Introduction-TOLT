931f15b8715b9614a38a29b3b677721989bac03c
Tim Toller <tim.toller05@htl-salzburg.ac.at>
2020-11-09 14:12:36 +0100

    Added ExampleClass.cs

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,28 @@
+﻿// Bibliotheken / Asembly Referenz / Namespace
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+//deklaration einer klasse
+
+//public        -> acces modifier (zugriffsrechte) | keyword (vorreservierte Schlüsselwort)
+//class         -> keyword zur Deklaration einer Klasse
+//ExampleClass  -> idetnitfier (Name der Klasse)
+//:MonoBehaviour-> Unterklasse von MonoBehaviour
+public class ExampleClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
